f800de4b0960a56f8bea86fa347e6a2d606cf0fb
Pranav Rangate <pranavrangate231104@gmail.com>
2024-04-22 22:35:16 +0530

    simple example

---
+++ linked_list_example.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+class LinkedList {
+private:
+    Node* head;
+
+public:
+    LinkedList() : head(nullptr) {}
+
+    void insertAtBeginning(int value) {
+        Node* newNode = new Node;
+        newNode->data = value;
+        newNode->next = head;
+        head = newNode;
+    }
+
+    void display() {
+        Node* temp = head;
+        while (temp != nullptr) {
+            std::cout << temp->data << " -> ";
+            temp = temp->next;
+        }
+        std::cout << "nullptr" << std::endl;
+    }
+};
+
+int main() {
+    LinkedList myList;
+
+    myList.insertAtBeginning(10);
+    myList.insertAtBeginning(20);
+    myList.insertAtBeginning(30);
+
+    myList.display();
+
+    return 0;
+}
